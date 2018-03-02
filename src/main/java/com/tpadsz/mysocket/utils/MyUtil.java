4c8776d8ecc866ac7a9458fe3404c42a4d21e103
hongjian.chen <766256898@qq.com>
2018-03-02 18:22:54 +0800

    非阻塞通信

--- src/main/java/com/tpadsz/mysocket/utils/MyUtil.java
+++ src/main/java/com/tpadsz/mysocket/utils/MyUtil.java
@@ -1,8 +1,36 @@
 package com.tpadsz.mysocket.utils;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.SocketChannel;
+
+
 /**
  * Created by hongjian.chen on 2018/2/28.
  */
 public class MyUtil {
+    public static void closeChannel(SelectionKey key) {
+        if (key != null) {
+            key.cancel();
+            try {
+                key.channel().close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public static void receive(SelectionKey key) throws IOException {
+        ByteBuffer buffer = (ByteBuffer) key.attachment();
+        SocketChannel socketChannel = (SocketChannel) key.channel();
+        ByteBuffer readBuff = ByteBuffer.allocate(32);
+        socketChannel.read(readBuff);
+        readBuff.flip();
+
+        buffer.limit(buffer.capacity());
+        buffer.put(readBuff);
+    }
 
 }
